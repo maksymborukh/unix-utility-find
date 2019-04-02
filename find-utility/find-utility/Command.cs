8964a64935e77d8c8eabc45f4be1ae7e8e5e13cb
Maksym Borukh <maksboruh@gmail.com>
2019-04-02 16:45:04 +0300

    add class command

---
+++ find-utility/find-utility/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_utility
+{
+    class Command
+    {
+    }
+}
