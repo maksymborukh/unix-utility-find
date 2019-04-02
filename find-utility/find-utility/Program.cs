0a0527dbb0b6e7bf6dd282d61e7ce2d7d06d01da
Maksym Borukh <maksboruh@gmail.com>
2019-04-02 16:37:22 +0300

    add solution

---
+++ find-utility/find-utility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
