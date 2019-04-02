1acb2b2c1640fc5e4674061a4c3ac3f69c52e836
Maksym Borukh <maksboruh@gmail.com>
2019-04-02 17:35:04 +0300

    add command recognition

--- find-utility/find-utility/Command.cs
+++ find-utility/find-utility/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,49 @@ namespace find_utility
 {
     class Command
     {
+        private Find find;
+        private Cd cd;
+        private string HomeDirectoryName;
+        private string DirectoryName;
+        
+
+        public Command()
+        {
+            find = new Find();
+            cd = new Cd();
+            HomeDirectoryName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+
+        public void ReadCommand()
+        {
+            string command;
+            do
+            {
+                Console.Write(HomeDirectoryName + ">");
+                command = Console.ReadLine();
+                ParseCommand(command);
+            } while (command != "exit");
+        }
+
+        private void ParseCommand(string c)
+        {
+            string[] utility = c.Split(' ');
+            string command = utility[0];
+
+            RecognizeCommand(command);
+        }
+
+        private void RecognizeCommand(string command)
+        {
+            switch (command)
+            {
+                case "find":
+                    break;
+                default:
+                    Console.WriteLine("'" + command + "' is not recognized as an internal or external command, operable program or batch file.");
+                    break;
+
+            }
+        }
     }
 }
